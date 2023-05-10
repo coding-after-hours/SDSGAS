d5040ac96786e31429a7e79f363aaee3ccb15641
Brandon-Schefstad <bschefstad@gmail.com>
2023-05-10 15:50:41 -0400

    Moved index to /src and adjusted stack

--- lib/sdsgas-stack.ts
+++ lib/sdsgas-stack.ts
@@ -1,6 +1,6 @@
 import * as cdk from 'aws-cdk-lib';
 
-import * as lambda from 'aws-cdk/aws-lambda-nodejs';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
 import * as path from 'path';
 
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
@@ -9,8 +9,10 @@ export class SdsgasStack extends cdk.Stack {
 	constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
 		super(scope, id, props);
 
-		const helloLambda = new lambda.NodejsFunction(this, 'MyLambda', {
-			entry: path.join(__dirname, '../functions/index.ts'),
+		const fn = new lambda.Function(this, 'MyFunction', {
+			runtime: lambda.Runtime.NODEJS_18_X,
+			handler: 'index.handler',
+			code: lambda.Code.fromAsset(path.join(__dirname, '../src/')),
 		});
 	}
 }
